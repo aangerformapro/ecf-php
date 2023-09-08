e8d34e4f07d14c7673c85b802da4bed608ffa40b
Aymeric Anger <a.anger@codeur.online>
2023-09-08 14:18:06 +0200

    ecf wordpress

---
+++ theme-wordpress/functions.php
@@ -0,0 +1,43 @@
+<?php
+function creer_type_de_contenu_avis()
+{
+    $labels = [
+        'name'               => 'mes avis',
+        'singular_name'      => 'Avis',
+        'plural_name'        => 'mes avis',
+        'menu_name'          => 'Mes avis',
+        'add_new'            => 'Ajouter un avis',
+        'add_new_item'       => 'Ajouter un nouvel avis',
+        'edit_item'          => 'Modifier l\'avis',
+        'new_item'           => 'Nouvel avis',
+        'view_item'          => 'Voir l\'avis',
+        'view_items'         => 'Voir les avis',
+        'search_items'       => 'Rechercher des avis',
+        'not_found'          => 'Aucun avis trouvé',
+        'not_found_in_trash' => 'Aucun avis trouvé dans la corbeille',
+        'all_items'          => 'Tous les avis',
+        'archives'           => 'Archives des avis',
+        'attributes'         => 'Attributs des avis',
+    ];
+
+    $args   = [
+        'labels'        => $labels,
+        'public'        => true,
+        'menu_position' => 1,
+        'menu_icon'     => 'dashicons-star-empty', // Icône de l'administration (étoile)
+        'supports'      => ['title', 'editor', 'author', 'thumbnail', 'custom-fields'],
+    ];
+
+    register_post_type('avis', $args);
+}
+add_action('init', 'creer_type_de_contenu_avis');
+
+function modifier_titre_archive_avis($title)
+{
+    if (is_post_type_archive('avis'))
+    {
+        $title = 'Mes avis';
+    }
+    return $title;
+}
+add_filter('post_type_archive_title', 'modifier_titre_archive_avis');
